3f6eda7c6825abc7f8984e56f3e0f2862a834a57
mugdha <mugdhasonawane88@gmail.com>
2022-07-05 17:43:02 +0530

    Feature-Tic-Tac-Toe: Created UI to render board for game

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './App.css';
 import { Constants } from './constants/Constants';
+import TicTacToe  from './components/TicTacToe';
+
 
 function App() {
   return (
     <div className="App">
     <header className="App-header">
-      <h1>Tic Tac Toe</h1>
+      <h1>{Constants.HEADER}</h1>
+      <TicTacToe />
     </header>
   </div>
   );
--- src/components/TicTacToe.js
+++ src/components/TicTacToe.js
@@ -1,12 +1,56 @@
-import './App.css';
-import { Constants } from './constants/Constants';
+import React, { useState } from 'react';
+import './TicTacToe.css';
 
 function TicTacToe() {
+
+  const [board, setBoard] = useState([
+    ["", "", ""],
+    ["", "", ""],
+    ["", "", ""],
+  ]);
+  console.log('board',typeof(board[1][0]));
+
+  const playerOneFn = () => {
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>{Constants.HEADER}</h1>
-      </header>
+    <div className="tictactoe">
+      <div className="container">
+        <div className="col">
+          <span role="cell" onClick={() => playerOneFn(0, 0)} className="cell">
+            {board[0][0]}
+          </span>
+          <span role="cell" onClick={() => playerOneFn(0, 1)} className="cell">
+            {board[0][1]}
+          </span>
+          <span role="cell" onClick={() => playerOneFn(0, 2)} className="cell">
+            {board[0][2]}
+          </span>
+        </div>
+        <div className="col">
+          <span role="cell" onClick={() => playerOneFn(1, 0)} className="cell">
+            {board[1][0]}
+          </span>
+          <span role="cell" onClick={() => playerOneFn(1, 1)} className="cell">
+            {board[1][1]}
+          </span>
+          <span role="cell" onClick={() => playerOneFn(1, 2)} className="cell">
+            {board[1][2]}
+          </span>
+        </div>
+        <div className="col">
+          <span role="cell" onClick={() => playerOneFn(2, 0)} className="cell">
+            {board[2][0]}
+          </span>
+          <span role="cell" onClick={() => playerOneFn(2, 1)} className="cell">
+            {board[2][1]}
+          </span>
+          <span role="cell" onClick={() => playerOneFn(2, 2)} className="cell">
+            {board[2][2]}
+          </span>
+        </div>
+      </div>
     </div>
   );
 }
